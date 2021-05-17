6dfd5af9c6dee8eb6fa76f6cb904c256ca053f7b
Jordan Caley <jordancaley10@gmail.com>
2021-05-17 11:34:27 +0200

    Add email and password validation to User model

--- models/User.js
+++ models/User.js
@@ -5,16 +5,31 @@ const UserSchema = new Schema(
   {
     firstName: String,
     lastName: String,
-    emailAddress: String,
-    phoneNumber: String,
-    streetAddress: String,
-    city: String,
-    zipCode: String,
+    // code for password and email taken from day-3 simple-auth-app example
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      validate: (email) => {
+        return Boolean(
+          email.match(
+            /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+          )
+        );
+      },
+    },
     photo: {
       type: String,
       default:
         "https://thumbs.dreamstime.com/b/default-avatar-profile-icon-social-media-user-vector-default-avatar-profile-icon-social-media-user-vector-portrait-176194876.jpg",
     },
+    phoneNumber: String,
+    streetAddress: String,
+    city: String,
+    zipCode: String,
     description: String,
     bankName: String,
     bankAccountNumber: String,
