1c3e6acc4af91a70aed42407113b64d15c9d8e98
Jordan Caley <jordancaley10@gmail.com>
2021-05-19 10:06:07 +0200

    Back to product link

--- routes/user.js
+++ routes/user.js
@@ -15,13 +15,13 @@ router.get("/product/:id", async (req, res, next) => {
     });
 
 //GET - display the vendor's details (from a product page)
-router.get("/vendor/:id", async (req, res, next) => {
+router.get("/vendor/:id", async (req, res, next) => { 
     const vendor = await UserModel.findById(req.params.id)//go into the user model and find the user that has this id (req.params.id)
     const products = await ProductModel.find({vendorId:req.params.id})// go into the product model and find all the products whose the vendorId equals this id (req.params.id)
     console.log(vendor);
     console.log(products);
     try {
-      res.render("vendorpage.hbs", { vendor, products });
+      res.render("vendorpage.hbs", { vendor, products, prodId: req.query.productid });
     } catch (err) {
       next(err);
     }
